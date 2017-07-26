b02d74f3c7b9df189a5a882a176c6489839e4721
Pyry Kontio <pyry.kontio@drasa.eu>
2017-07-26 20:41:47 +0900

    Made the exact breaking nightlies crystal clear.

--- build_musl_stable.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-docker run -it --rm -v $PWD:/workdir -v ~/.cargo/git:/root/.cargo/git -v ~/.cargo/registry:/root/.cargo/registry golddranks/rust_musl_stable_docker:latest cargo build --release --target=x86_64-unknown-linux-musl -vv
---
+++ build_musl_works.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+cargo clean
+docker run -it --rm -v $PWD:/workdir -v ~/.cargo/git:/root/.cargo/git -v ~/.cargo/registry:/root/.cargo/registry golddranks/rust_musl_docker:nightly-2017-07-19 cargo build --release --target=x86_64-unknown-linux-musl -vv
